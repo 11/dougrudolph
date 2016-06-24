c9a2302132be35650b344d69565eed7d02052040
Douglas Rudolph <drudolph@gmail.com>
2016-06-24 06:10:40 -0400

    taking website in new direction, starting to get the basic layout

--- js/PageTyper.js
+++ js/PageTyper.js
@@ -1,17 +1,1 @@
 
-function write(currentText, elementId, delay)
-{
-  var element = document.getElementById(elementId);
-  var charCount=0;
-
-  setInterval(function(){
-    if(charCount>currentText.length)
-    {
-      return;
-    }
-    else
-    {
-      element.innerHTML = currentText.substr(0,charCount++);
-    }
-  }, delay);
-}
