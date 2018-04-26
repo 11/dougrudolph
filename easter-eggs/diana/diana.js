fe51c028f6cfa75002ff435d9466eaca204d6d5a
douglas rudolph <douglas.rudolph@buzzfeed.com>
2018-04-26 10:38:55 -0400

    udpating visit times

--- easter-eggs/diana/diana.js
+++ easter-eggs/diana/diana.js
@@ -30,9 +30,9 @@ function timer(msg, elementId, countDownDate){
     }
 
 window.onload = function(){
-    timer("Next visit: ", "NV", "Apr 20, 2018 19:30:00");
+    timer("Next visit: ", "NV", "Apr 28, 2018 19:30:00");
     timer("10 Months: ", "MA", "May 3, 2018 12:00:00");
-    timer("1 Year: ", "OY", "Aug 3, 2018 12:00:00");
+    timer("1 Year: ", "OY", "Jul 3, 2018 12:00:00");
     timer("End of semester: ", "EOS", "May 9, 2018 15:37:25");
 }
 
