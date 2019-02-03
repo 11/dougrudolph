72ba17eb41f82268783d794267cd1a7a2b49a245
11 <drudolph914@gmail.com>
2019-02-02 21:48:49 -0500

    updating the title of the page and getting walls to work

--- blogs/line-of-sight/line-of-sight.js
+++ blogs/line-of-sight/line-of-sight.js
@@ -13,123 +13,45 @@ window.onload  = function(){
     var MOUSE_Y = canvas.height / 2 - 2;
     var COUNT = 0;
 
-    class Cell{
 
-        constructor(exists){
-            this.exists = exists;
-            this.edgeId = {'north': -1, 'south': -1, 'east': -1, 'west': -1};
-            this.edgeExists = {'north': false, 'south': false, 'east': false, 'west': false};
-        }
+    class Wall{
 
-        exists(){
-            return this.exists;
-        }
-    }
+        constructor(lineList){
 
-    class TileMap{
-
-        constructor(){
-            this.MAP = [
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-            ];
-
-            this.TILE_MAP = []
-            this.edgePool = {};
-
-            // centers the tilemap in the canvas
-            this.offsetX = (canvas.width / 2) - (this.TILE_SIZE*this.MAP[0].length/2);
-            this.offsetY = (canvas.height / 2) - (this.TILE_SIZE*this.MAP.length/2);
-            this.TILE_SIZE = canvas.width*0.03;
-
-            // create a tilemap filled with cell objects based on the array version of the map
-            this.initTileMap();
-        }
-
-        initTileMap(){
-            for(let i=0; i<this.MAP.length; i++){
-
-                // create array in tilemap
-                this.TILE_MAP[i] = new Array(this.MAP[i].length);
-
-                for(let j=0; j<this.MAP[i].length; j++){
-                    if(this.MAP[i][j] == 1){
-                        this.TILE_MAP[i][j] = new Cell(true);
-                    }
-                    else{
-                        this.TILE_MAP[i][j] = new Cell(false);
-                    }
-                }
-
-            }
-        }
-
-        findVertices(){
-            for(let i=1; i<this.MAP.length-1; i++){
-                for(let j=1; j<this.MAP[i].length-1; j++){
-
-                    // store reference to current element
-                    let CUR   = this.TILE_MAP[i][j];
-
-                    // store references to the neighbors in each cardinal direction
-                    let NORTH = this.TILE_MAP[i-1][j];
-                    let SOUTH = this.TILE_MAP[i+1][j];
-                    let EAST  = this.TILE_MAP[i][j+1];
-                    let WEST  = this.TILE_MAP[i][j-1];
-
-                    //if cell exists
-                    if(CUR.exists()){
-
-                        // extend the NORTH's western edge down
-                        if(!WEST.exists() && NORTH.exists()){
-
-                        }
-
-                        //
-                        else if(!WEST.exists() && !NORTH.exists()){
-                            CUR.edgeExists['west'] = true;
-                            CUR.edgeId['west']     = COUNT;
-                            this.edgePool[COUNT] = CUR;
-                        }
-
-                    }
-                }
+            if(lineList.length < 2){
+                throw "Line List length has less than 2 elements";
+                return;
             }
 
+            // list of [x,y] coordinates as a set of lists
+            this.lineList = lineList;
+
+            // color of the walls
+            this.lineColor = "#F0F0F0";
         }
 
         draw(ctx){
+            var start, end, startX, startY, endX, endY;
 
-            let tileColor = '#F0F0F0';
+            for(let i = 0; i < this.lineList.length-1; i++){
+                start = this.lineList[i];
+                end = this.lineList[i+1];
 
-            for(let i=0; i < this.MAP.length; i++){
-                for(let j=0; j < this.MAP[i].length; j++){
+                startX = start[0];
+                startY = start[1];
+                endX = end[0];
+                endY = end[1];
 
-                    if(this.MAP[i][j] == 1){
+                console.log('startX:' + startX + '\nstartY' + startY);
+                console.log('endX:' + endX + '\nendY' + endY);
 
-                        let x = j*this.TILE_SIZE + this.offsetX;
-                        let y = i*this.TILE_SIZE + this.offsetY;
-
-                        ctx.beginPath();
-                        ctx.fillStyle = tileColor;
-                        ctx.rect(x, y, this.TILE_SIZE, this.TILE_SIZE);
-                        ctx.fill();
-                        ctx.closePath();
-
-                    }
-                }
+                ctx.beginPath();
+                ctx.strokeStyle = this.lineColor;
+                ctx.moveTo(startX, startY);
+                ctx.lineTo(endX, endY);
+                ctx.stroke();
             }
         }
-
-        update(lightsource){
-        }
-
     }
 
     class LightSource{
@@ -153,20 +75,21 @@ window.onload  = function(){
             this.x = MOUSE_X;
             this.y = MOUSE_Y;
         }
-
     }
 
     let lightSource = new LightSource(canvas.width/2, canvas.height/2, 5);
-    let tileMap = new TileMap();
+    let wall1 = new Wall([[50,180], [120,120], [200, 130], [250,90]]);
 
     function run(){
         //clear previous frame
         ctx.clearRect(0,0,canvas.width, canvas.height);
 
         //update
+        //
         //render
         lightSource.draw(ctx);
-        tileMap.draw(ctx);
+        wall1.draw(ctx);
+
     }
     setInterval(run, 30);
 
