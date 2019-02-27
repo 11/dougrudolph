96ea02cfe8c85bf9d2013f50c181c3d557b0d7bd
11 <drudolph914@gmail.com>
2019-02-27 18:21:25 -0500

    updating resume link and updating homepage

--- blogs/line-of-sight/line-of-sight.js
+++ blogs/line-of-sight/line-of-sight.js
@@ -21,31 +21,34 @@ window.onload  = function(){
     class EdgePool{
 
         constructor(wallList){
-           this.edgePool = this.initEdgePool(wallList);
+            this.edgePool = this.initEdgePool(wallList);
+            this.wallList = wallList;
         }
 
         initEdgePool(wallList){
             let edgePool = [];
 
+            let wallLines, line;
             for(let i=0; i<wallList.length; i++){
-                for(let j=0; j<wallList[i].length; j++){
-                    this.edgeList.push(wallList[i][j]);
+                wallLines = wallList[i].lineList;
+
+                for(let j=0; j<wallLines.length; j++){
+                    line = wallLines[j];
+                    edgePool.push(line);
                 }
             }
+
             return edgePool;
         }
 
         draw(ctx){
             let wall;
-            for(let i=0; i<this.edgePool.length; i++){
-                wall = this.edgePool[i];
+            for(let i=0; i<this.wallList.length; i++){
+                wall = this.wallList[i];
                 wall.draw(ctx);
             }
         }
-
     }
-
-
 
     class Wall{
 
@@ -75,9 +78,6 @@ window.onload  = function(){
                 endX = end[0];
                 endY = end[1];
 
-                console.log('startX:' + startX + '\nstartY' + startY);
-                console.log('endX:' + endX + '\nendY' + endY);
-
                 ctx.beginPath();
                 ctx.strokeStyle = this.lineColor;
                 ctx.moveTo(startX, startY);
@@ -93,16 +93,50 @@ window.onload  = function(){
             this.x = x;
             this.y = y;
             this.color = "#F0F0F0";
+            this.rayColor = "cyan";
             this.radius = radius;
         }
 
         draw(ctx, edgePool){
+
+            // draw ball representing the lightsource
             ctx.beginPath();
             ctx.fillStyle = this.color;
             ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
             ctx.fill();
             ctx.closePath();
 
+            // draw lines from the ball to the vectors
+            let edge, edgeX, edgeY;
+            for(let i=0; i<edgePool.edgePool.length; i++){
+
+                edge = edgePool.edgePool[i];
+                edgeX = edge[0];
+                edgeY = edge[1];
+
+                ctx.beginPath();
+                ctx.strokeStyle = this.rayColor;
+                ctx.moveTo(this.x, this.y);
+                ctx.lineTo(edgeX, edgeY);
+                ctx.stroke();
+            }
+        }
+
+        update(edgePool){
+
+            let edge;
+            for(let i=0; i<edgePool.edgePool.length; i++){
+                edge = edgePool.edgePool[i];
+                let edgeX = edge[0];
+                let edgeY = edge[1];
+
+                let originX = this.x;
+                let originY = this.y;
+
+                let dx = edgeX - originX;
+                let dy = edgeY - originY;
+                let baseAngle = Math.atan2(dx, dy);
+            }
         }
 
         updatePos(){
@@ -112,8 +146,7 @@ window.onload  = function(){
     }
 
     // set of walls
-    let wall1 = new Wall([[50,180], [120,120], [200, 130], [250,90]]);
-
+    let wall1 = new Wall([[50,180], [120,120], [200, 90], [250,90]]);
 
     // main objects for interacting with the simulation
     let lightSource = new LightSource(canvas.width/2, canvas.height/2, 5);
@@ -126,8 +159,8 @@ window.onload  = function(){
         // update
 
         // render
-        lightSource.draw(ctx);
         edgePool.draw(ctx);
+        lightSource.draw(ctx, edgePool);
 
     }
     setInterval(run, 30);
